ecf59b13f9ab2f01adeef40d010fa4efda51168e
jhidalgo-utep <jhidalgo.utep@gmail.com>
2023-05-09 19:49:52 -0600

    added decription to main.py file

--- The Crypto Application/CryptoAppProject/src/main.py
+++ The Crypto Application/CryptoAppProject/src/main.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 """
+Project Name: The Crypto App Project
+Description: Custom GUI application for trading crypto on the Gemini Marketplace
+
+
 Created on Tue May  9 19:16:55 2023
 
 @author: joaquin
 """
+
+if __name__ == "__main__":
+    print('hiiii')
 
 
 
